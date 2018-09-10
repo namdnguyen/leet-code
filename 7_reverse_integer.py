0b053a81e7653107c9554f088d810ef6df3d98c2
Nam D. Nguyen <nnguyen83@gmail.com>
2018-09-10 11:35:20 -0500

    Add solution to "Reverse Integer" problem
    Submission accepted. Runtime beat 99.91% of Python3 submissions!
    
    Runtime: 52 ms

--- 7_reverse_integer.py
+++ 7_reverse_integer.py
@@ -29,3 +29,18 @@ class Solution:
 
         URL: https://leetcode.com/problems/reverse-integer/description/
         """
+        is_negative = bool(x < 0)
+        string = str(abs(x))
+        string_list = list(reversed(string))
+        reversed_string = "".join(string_list)
+
+        if is_negative is True:
+            reversed_int = int(reversed_string) * -1
+        else:
+            reversed_int = int(reversed_string)
+
+        # Check if reversed integer overflows
+        if reversed_int > 2**31 or reversed_int < -2**31:
+            reversed_int = 0
+
+        return reversed_int
