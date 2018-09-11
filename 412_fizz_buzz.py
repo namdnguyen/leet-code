dab7b2aaf1763ff3beeda9bb63291030d1718c37
Nam D. Nguyen <nnguyen83@gmail.com>
2018-09-11 08:56:17 -0500

    Add solution to Fizz Buzz problem
    Solution accepted by Leet Code. Runtime faster than 78.22% of python3
    submissions.
    
    Runtime: 56 ms

--- 412_fizz_buzz.py
+++ 412_fizz_buzz.py
@@ -36,3 +36,16 @@ class Solution:
 
         URL: https://leetcode.com/problems/fizz-buzz/description/
         """
+        fizzbuzz = []
+
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                fizzbuzz.append("FizzBuzz")
+            elif i % 3 == 0:
+                fizzbuzz.append("Fizz")
+            elif i % 5 == 0:
+                fizzbuzz.append("Buzz")
+            else:
+                fizzbuzz.append(str(i))
+
+        return fizzbuzz
